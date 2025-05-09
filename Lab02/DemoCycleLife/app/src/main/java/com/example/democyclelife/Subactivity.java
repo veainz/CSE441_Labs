388f2aec4e84c35398e785daf246f51af36400a6
veainz <bdluong.hubnetwork@gmail.com>
2025-05-09 18:14:55 +0700

    Lab02_For1stTime
    UndoneLab02

---
+++ Lab02/DemoCycleLife/app/src/main/java/com/example/democyclelife/Subactivity.java
@@ -0,0 +1,29 @@
+package com.example.democyclelife;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class subactivity extends AppCompatActivity {
+    Button btnok;
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+        btnok = findViewById(R.id.btnok);
+        btnok.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                finish();
+            }
+        });
+    }
+}
