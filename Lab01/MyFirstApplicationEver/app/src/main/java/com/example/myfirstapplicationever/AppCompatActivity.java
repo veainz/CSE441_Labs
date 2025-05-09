566bc78060069e69f15f58e6a264a336ad6088e0
veainz <bdluong.hubnetwork@gmail.com>
2025-05-09 17:44:35 +0700

    firstcommitforlab01
    Likedocumentory

---
+++ Lab01/MyFirstApplicationEver/app/src/main/java/com/example/myfirstapplicationever/AppCompatActivity.java
@@ -0,0 +1,9 @@
+package com.example.myfirstapplicationever;
+
+import android.os.Bundle;
+
+public class AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        
+    }
+}
---
+++ Lab01/MyFirstApplicationEver/app/src/main/java/com/example/myfirstapplicationever/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.myfirstapplicationever;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtA, edtB, edtKQ;
+    Button btncong;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtA = findViewById(R.id.edtA);
+        edtB = findViewById(R.id.edtB);
+        edtKQ = findViewById(R.id.edtKQ);
+        btncong = findViewById(R.id.btntong);
+
+        btncong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a = Integer.parseInt(edtA.getText().toString());
+                int b = Integer.parseInt(edtB.getText().toString());
+                int c = a + b;
+                edtKQ.setText(String.valueOf(c));
+            }
+        });
+    }
+}
