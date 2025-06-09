8a1deeb14b3ee79f4b5a38d54bd80f55d7d8b59b
veainz <bdluong.hubnetwork@gmail.com>
2025-06-09 09:46:23 +0700

    TheCompleteLab02
    Done Lab02

--- Lab02/DemoCycleLife/app/src/main/java/com/example/democyclelife/MainActivity.java
+++ Lab02/DemoCycleLife/app/src/main/java/com/example/democyclelife/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.democyclelife;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,57 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    Button btncall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Toast.makeText(this, "CR424 - onCreat()", Toast.LENGTH_SHORT).show();
+        btncall = findViewById(R.id.btncall);
+        btncall.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this, subactivity.class);
+                startActivity(intent1);
+            }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this," CR424 - onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this," CR424 - onRestart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this," CR424 - onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this," CR424 - onPause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this," CR424 - onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this,"CR424 - on Destroy()",Toast.LENGTH_SHORT).show();
     }
 }
--- Lab02/DemoCycleLife/app/src/main/java/com/example/democyclelife/subactivity.java
+++ Lab02/DemoCycleLife/app/src/main/java/com/example/democyclelife/subactivity.java
@@ -11,12 +11,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class subactivity extends AppCompatActivity {
+    
     Button btnok;
 
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sub);
+        setContentView(R.layout.activity_subactivity);
         btnok = findViewById(R.id.btnok);
         btnok.setOnClickListener(new View.OnClickListener() {
 
