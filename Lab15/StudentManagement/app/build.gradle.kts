4fd3451914aee98ea7e1dd58ecad97c65a4167e4
veainz <bdluong.hubnetwork@gmail.com>
2025-06-13 11:03:24 +0700

    Fixed15

--- Lab15/StudentManagement/app/build.gradle.kts
+++ Lab15/StudentManagement/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.studentmanagement"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.studentmanagement"
